8d3284a225c670c848d956f94a5b5ee7b03a030c
Hiromant31 <jobautoopros@gmail.com>
2026-05-01 04:34:48 +0300

    fhs

--- src/main.py
+++ src/main.py
@@ -154,4 +154,4 @@ if __name__ == "__main__":
         public_url = ngrok.connect(8000)
         print(f"🌍 Твой интерфейс доступен по ссылке: {public_url}")
         
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
